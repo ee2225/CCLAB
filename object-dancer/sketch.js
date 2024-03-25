add71c7a76b38545c4926077de8872efd5d9a10b
ee2225 <ee2225@nyu.edu>
2024-03-25 12:26:27 +0800

    upload the project files

--- object-dancer/sketch.js
+++ object-dancer/sketch.js
@@ -1,23 +1,9 @@
-/*
-  Check our the GOAL and the RULES of this exercise at the bottom of this file.
-  
-  After that, follow these steps before you start coding:
-
-  1. rename the dancer class to reflect your name (line 35).
-  2. adjust line 20 to reflect your dancer's name, too.
-  3. run the code and see if a square (your dancer) appears on the canvas.
-  4. start coding your dancer inside the class that has been prepared for you.
-  5. have fun.
-*/
-
 let dancer;
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("p5-canvas-container");
-
-  // ...except to adjust the dancer's name on the next line:
-  dancer = new YourNameDancer(width / 2, height / 2);
+  dancer = new angryBird(width / 2, height / 2);
 }
 
 function draw() {
@@ -28,73 +14,123 @@ function draw() {
   dancer.display();
 }
 
-// You only code inside this class.
-class YourNameDancer {
+class angryBird {
   constructor(startX, startY) {
     this.x = startX;
     this.y = startY;
-    // add properties for your dancer here:
-    //..
-    //..
-    //..
+    this.eyeX = 0;
+    this.browY = 0;
+    this.beakY = 0;
   }
+
   update() {
-    // update properties here to achieve
-    // your dancer's desired moves and behaviour
+    this.x += 1.5*sin(frameCount / 6);
+    this.y += 3 * cos(frameCount * 0.04);
+    this.eyeX += cos(frameCount * 0.15);
+    this.browY += -cos(frameCount * 0.2);
+    this.beakY += sin(frameCount * 0.3);
   }
+
   display() {
-    // the push and pop, along with the translate 
-    // places your whole dancer object at this.x and this.y.
-    // you may change its position on line 19 to see the effect.
     push();
     translate(this.x, this.y);
-
-    // ******** //
     // ⬇️ draw your dancer from here ⬇️
 
+    //tails
+    fill("darkred");
+    stroke(0);
+    strokeWeight(1);
+    push();
+    rotate(sin(frameCount * 0.1) * 0.2);
+    beginShape();
+    vertex(-93, 5);
+    vertex(-50, 10);
+    vertex(-50, 15);
+    vertex(-95, 20);
+    vertex(-93, 5);
+    endShape();
 
+    beginShape();
+    vertex(-78, -19);
+    vertex(-50, 10);
+    vertex(-50, 10);
+    vertex(-85, -5);
+    vertex(-78, -20);
+    endShape();
 
+    beginShape();
+    vertex(-85, 30);
+    vertex(-50, 15);
+    vertex(-50, 20);
+    vertex(-80, 40);
+    endShape();
+    pop();
 
+    //hair
+    fill("red");
+    strokeWeight(4);
+    push();
+    translate(30, -20);
+    rotate(-sin(1.5));
+    ellipse(0, -65, 20, 50);
+    ellipse(15, -65, 20, 50);
+    pop();
 
+    //body
+    push();
+    circle(10, 15, 150);
+
+    //eyes
+    fill(255);
+    strokeWeight(3);
+    circle(52, 15, 31);
+    circle(22, 15, 33);
+    fill(0);
+    circle(50 + this.eyeX, 15, 9);
+    circle(27 + this.eyeX, 15, 10);
+
+    //eyebrows
+    beginShape();
+    vertex(0, -15 + this.browY);
+    vertex(40, 0);
+    vertex(40, 10);
+    vertex(2, 0 + this.browY);
+    vertex(0, -15 + this.browY);
+    endShape();
+
+    beginShape();
+    vertex(40, 0);
+    vertex(80, -15 + this.browY);
+    vertex(82, 0 + this.browY);
+    vertex(40, 10);
+    vertex(40, 0);
+    endShape();
+
+    //belly
+    fill(221, 184, 146);
+    noStroke();
+    ellipse(10, 65, 95, 45);
+
+    //beak
+    strokeWeight(3);
+    stroke(0);
+    fill(255, 195, 0);
+    triangle(39, 20, 70, 47 - this.beakY, 19, 45);
+    triangle(19, 45, 38, 65, 63, 46 + this.beakY);
 
     // ⬆️ draw your dancer above ⬆️
-    // ******** //
-
-    // the next function draws a SQUARE and CROSS
-    // to indicate the approximate size and the center point
-    // of your dancer.
-    // it is using "this" because this function, too, 
-    // is a part if your Dancer object.
-    // comment it out or delete it eventually.
-    this.drawReferenceShapes()
-
+   
+    //this.drawReferenceShapes()
     pop();
   }
-  drawReferenceShapes() {
-    noFill();
-    stroke(255, 0, 0);
-    line(-5, 0, 5, 0);
-    line(0, -5, 0, 5);
-    stroke(255);
-    rect(-100, -100, 200, 200);
-    fill(255);
-    stroke(0);
-  }
+  // drawReferenceShapes() {
+  //   noFill();
+  //   stroke(255, 0, 0);
+  //   line(-5, 0, 5, 0);
+  //   line(0, -5, 0, 5);
+  //   stroke(255);
+  //   rect(-100, -100, 200, 200);
+  //   fill(255);
+  //   stroke(0);
+  // }
 }
-
-
-
-/*
-GOAL:
-The goal is for you to write a class that produces a dancing being/creature/object/thing. In the next class, your dancer along with your peers' dancers will all dance in the same sketch that your instructor will put together. 
-
-RULES:
-For this to work you need to follow one rule: 
-  - Only put relevant code into your dancer class; your dancer cannot depend on code outside of itself (like global variables or functions defined outside)
-  - Your dancer must perform by means of the two essential methods: update and display. Don't add more methods that require to be called from outside (e.g. in the draw loop).
-  - Your dancer will always be initialized receiving two arguments: 
-    - startX (currently the horizontal center of the canvas)
-    - startY (currently the vertical center of the canvas)
-  beside these, please don't add more parameters into the constructor function 
-  - lastly, to make sure our dancers will harmonize once on the same canvas, please don't make your dancer bigger than 200x200 pixels. 
-*/
